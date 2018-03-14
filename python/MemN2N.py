f101b291be41465b2b35315f35ed4519905252ab
lacuca <mulikea@gmail.com>
2018-03-14 19:35:27 +0900

    test
    test

--- python/MemN2N.py
+++ python/MemN2N.py
@@ -11,7 +11,7 @@ test = 1  # TEST 할 문제 번호
 stddev = 0.1  # 표준 편차
 learning_rate = 0.01  # 학습률
 _WORD = 20  # 사전의 크기 (사전이 저장할 수 있는 최대 WORD)
-_MEMORY = 150  # Memory Vector 의 크기
+_MEMORY = 177  # Memory Vector 의 크기
 #################
 print("[Constant]", "test: {}, stddev: {}, learning_rate: {}, WORD: {}, MEMORY: {}"
       .format(test, stddev, learning_rate, _WORD, _MEMORY))
